e398056e2e179a5340ceaf5ca0ef83a34483a4bc
G <griehm101@gmail.com>
2015-01-29 15:39:54 -0800

    First commit of StockWatcher app

---
+++ test/com/google/gwt/sample/stockwatcher/client/StockWatcherTest.java
@@ -0,0 +1,25 @@
+package com.google.gwt.sample.stockwatcher.client;
+
+import com.google.gwt.junit.client.GWTTestCase;
+
+/**
+ * GWT JUnit tests must extend GWTTestCase.
+ */
+public class StockWatcherTest extends GWTTestCase {
+
+  /**
+   * Must refer to a valid module that sources this class.
+   */
+  @Override
+public String getModuleName() {
+    return "com.google.gwt.sample.stockwatcher.StockWatcher";
+  }
+
+  /**
+   * Add as many tests as you like.
+   */
+  public void testSimple() {
+    assertTrue(true);
+  }
+
+}
